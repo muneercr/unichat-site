54d5905a52fe7db7941807fca6f628fb8f463af6
muneercr <muneercr077@gmail.com>
2022-04-09 17:04:15 +0530

    Updates

---
+++ precache-manifest.85d9ffe3ae22de437b1cb28160957abc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384dae6b2d4a219cf87da910cbbece5d",
+    "url": "/github.io.unichat-site/index.html"
+  },
+  {
+    "revision": "2cf274310b7b7907faca",
+    "url": "/github.io.unichat-site/static/css/main.906d9089.chunk.css"
+  },
+  {
+    "revision": "194289e6335d0ad1533f",
+    "url": "/github.io.unichat-site/static/js/2.55a0d150.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/github.io.unichat-site/static/js/2.55a0d150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf274310b7b7907faca",
+    "url": "/github.io.unichat-site/static/js/main.e16b5f2a.chunk.js"
+  },
+  {
+    "revision": "09c6aef1d0665e0c3e23",
+    "url": "/github.io.unichat-site/static/js/runtime-main.6543e77f.js"
+  }
+]);
